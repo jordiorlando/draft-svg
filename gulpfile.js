8ab86f94f302bfe3a47102b6e60a446770d4a0fc
Jordi Orlando <jordi.orlando@gmail.com>
2016-05-19 23:36:45 -0500

    Update gulpfile with UMD

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,40 @@ const rename = require('gulp-rename');
 const size = require('gulp-size');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
+const umd = require('gulp-umd');
 
 
 
 var pkg = require('./package.json');
 pkg.buildDate = Date();
 
+var name = 'draft-svg';
+var umdName = function() {
+  return 'draftSVG';
+};
+var dependencies = function() {
+  return ['draft'];
+};
+
 var src = 'draft-svg.js';
 
-var headerLong = [
-  '/*',
-  '* <%= pkg.name %> - <%= pkg.description %>',
-  '* version v<%= pkg.version %>',
-  '* <%= pkg.homepage %>',
-  '*',
-  '* copyright <%= pkg.author %>',
-  '* license <%= pkg.license %>',
-  '*',
-  '* BUILT: <%= pkg.buildDate %>',
-  '*/\n'
-].join('\n');
+var headerLong = `/*
+ * <%= pkg.name %> - <%= pkg.description %>
+ * version v<%= pkg.version %>
+ * <%= pkg.homepage %>
+ *
+ * copyright <%= pkg.author %>
+ * license <%= pkg.license %>
+ *
+ * BUILT: <%= pkg.buildDate %>
+ */
+
+`;
 
 var headerShort = [
-  '/*<%= pkg.name %> v<%= pkg.version %>',
+  '/* <%= pkg.name %> v<%= pkg.version %>',
   '<%= pkg.homepage %>',
-  '<%= pkg.license %> license*/\n'
+  'license <%= pkg.license %> */\n'
 ].join(' | ');
 
 
@@ -43,18 +52,29 @@ gulp.task('clean', function() {
 
 gulp.task('es6', ['clean'], function() {
   return gulp.src(src)
+    /* .pipe(umd({
+      dependencies,
+      exports: umdName,
+      namespace: umdName
+    })) */
     .pipe(rename({suffix: '-es6'}))
     .pipe(header(headerLong, {pkg: pkg}))
     .pipe(size({showFiles: true, title: 'Full'}))
     .pipe(gulp.dest('dist'));
 });
 
+// BACKLOG: figure out why sourcemaps don't exactly work
 gulp.task('build', ['clean'], function() {
   return gulp.src(src)
     .pipe(babel({
       plugins: ['transform-remove-console'],
       presets: ['es2015']
     }))
+    /* .pipe(umd({
+      dependencies,
+      exports: umdName,
+      namespace: umdName
+    })) */
     .pipe(header(headerLong, {pkg: pkg}))
     .pipe(size({showFiles: true, title: 'Full'}))
     .pipe(gulp.dest('dist'))
